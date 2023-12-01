761cd946d90f339b461d4b03fff888922b091cf9
peter-halligan <phally1980@gmail.com>
2023-12-01 12:57:04 +0100

    switched from sql lite to postgres added project

--- djangotodo/djangotodo/urls.py
+++ djangotodo/djangotodo/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('todos/', include("todo.urls"), name="todos"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- djangotodo/todo/views.py
+++ djangotodo/todo/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .forms import TodoForm
 from .models import TodoItem
 
 # list todos
+@login_required(login_url="/accounts/login/")
 def todos(request):
 
     context = {
-        'todos': TodoItem.objects.all(),
+        'todos': TodoItem.objects.filter(owner=request.user),
     }
     return render(request, "todos.html", context)
 
 # todo form
+@login_required(login_url="/accounts/login/")
 def todo_form(request, pk=None):
     if request.method == "POST":
         if pk:
             todo = get_object_or_404(TodoItem, pk=pk)
             form = TodoForm(request.POST, instance=todo)
+            form.instance.owner = request.user
 
         else:
             form = TodoForm(request.POST)
+            form.instance.owner = request.user
 
         if form.is_valid():
             form.save()
@@ -41,6 +48,7 @@ def todo_form(request, pk=None):
     return render(request, "todo_form.html", context)
 
 # details for a single todo
+@login_required(login_url="/accounts/login/")
 def todo(request, pk):
     todo = get_object_or_404(TodoItem, pk=pk)
     context = { 'todo': todo }
@@ -53,6 +61,7 @@ def todo(request, pk):
     
     return render(request, "todo.html", context)
 
+@login_required(login_url="/accounts/login/")
 def delete(request, pk):
     todo = get_object_or_404(TodoItem, pk=pk)
     todo.delete()
